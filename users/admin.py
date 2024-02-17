f495682dca7239b58cf6c55b221c927735e1850e
houdaaaaaaaaaa <nourgheffari@gmail.com>
2024-02-17 18:15:59 +0100

    1

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'num_bureau', 'is_staff', 'is_active')
+
+admin.site.register(CustomUser, CustomUserAdmin)
+
 
 
--- users/models.py
+++ users/models.py
@@ -2,5 +2,13 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class CustomUser(AbstractUser):
-    num_bureau = models.IntegerField()
+    num_bureau = models.IntegerField(default=0)
+
+    class Meta(AbstractUser.Meta):
+        # Add or change related_name for groups
+        permissions = [
+            ('view_user', 'Can view user'),
+        ]
+        db_table = 'custom_user'  # Change to a unique table name
+        swappable = 'AUTH_USER_MODEL'  # Add this line
 
