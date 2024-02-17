7c2dd2689ef3914ea6a29b1eb6fe3a5b5ff27c3f
houdaaaaaaaaaa <nourgheffari@gmail.com>
2024-02-17 23:38:57 +0100

    1

--- users/admin.py
+++ users/admin.py
@@ -2,18 +2,18 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
 
-from users.models import Employee
+from users.models import Localisation
 
 # Define an inline admin descriptor for Employee model
 # which acts a bit like a singleton
-class EmployeeInline(admin.StackedInline):
-    model = Employee
+class LocalisationInline(admin.StackedInline):
+    model = Localisation
     can_delete = False
-    verbose_name_plural = 'employee'
+    verbose_name_plural = 'localisation'
 
 # Define a new User admin
 class UserAdmin(BaseUserAdmin):
-    inlines = (EmployeeInline,)
+    inlines = (LocalisationInline,)
 
 # Re-register UserAdmin
 admin.site.unregister(User)
--- users/models.py
+++ users/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Employee(models.Model):
+class Localisation(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     department = models.CharField(max_length=100)
-    num_bureau=models.IntegerField()
+    numero_de_bureau=models.IntegerField()
