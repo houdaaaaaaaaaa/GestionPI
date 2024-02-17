1818aff4b906bc1a3c46dcc23c1c8060cf27269f
houdaaaaaaaaaa <nourgheffari@gmail.com>
2024-02-17 17:26:56 +0100

    1

--- users/models.py
+++ users/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    num_bureau= models.models.IntegerField()   
+    num_bureau= models.IntegerField()   
 def __str__(self):
         return self.user.username
     
