1e3bc129e2b21f6a4f19f72980272be2f159d4a7
houdaaaaaaaaaa <nourgheffari@gmail.com>
2024-02-17 21:24:55 +0100

    1

--- users/admin.py
+++ users/admin.py
@@ -1,11 +1,21 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 
-class CustomUserAdmin(UserAdmin):
-    list_display = ('username', 'email', 'num_bureau', 'is_staff', 'is_active')
+from users.models import Employee
 
-admin.site.register(CustomUser, CustomUserAdmin)
+# Define an inline admin descriptor for Employee model
+# which acts a bit like a singleton
+class EmployeeInline(admin.StackedInline):
+    model = Employee
+    can_delete = False
+    verbose_name_plural = 'employee'
 
+# Define a new User admin
+class UserAdmin(BaseUserAdmin):
+    inlines = (EmployeeInline,)
 
-
+# Re-register UserAdmin
+admin.site.register(Employee)
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
--- users/models.py
+++ users/models.py
@@ -1,14 +1,7 @@
-from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.contrib.auth.models import User
 
-class CustomUser(AbstractUser):
-    num_bureau = models.IntegerField(default=0)
-
-    class Meta(AbstractUser.Meta):
-        # Add or change related_name for groups
-        permissions = [
-            ('view_user', 'Can view user'),
-        ]
-        db_table = 'custom_user'  # Change to a unique table name
-        swappable = 'AUTH_USER_MODEL'  # Add this line
-
+class Employee(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    department = models.CharField(max_length=100)
+    num_bureau=models.IntegerField()
