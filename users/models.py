c365cf262335d3d51039359c47726336e7a2ccf9
houdaaaaaaaaaa <nourgheffari@gmail.com>
2024-02-17 17:44:16 +0100

    1

--- users/models.py
+++ users/models.py
@@ -1,9 +1,5 @@
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    num_bureau= models.IntegerField()   
-def __str__(self):
-        return self.user.username
-    
+class User(AbstractUser):
+  num_bureau= models.IntegerField()   
