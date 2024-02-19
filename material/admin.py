73afda264feb71ce7f10667e009f1263e4a35519
houdaaaaaaaaaa <nourgheffari@gmail.com>
2024-02-19 07:04:45 +0100

    1

--- material/admin.py
+++ material/admin.py
@@ -2,11 +2,13 @@ from django.contrib import admin
 from django.contrib.auth.models import User
 from users.models import Localisation
 from .models import Material,Famille_Material,Fournisseur
+
 class MaterialAdmin(admin.ModelAdmin):
     list_display = ('nom', 'numero_serie', 'marque', 'duree_garantie', 'duree_vie_debut', 'duree_vie_fin', 'date_affectation', 'statut', 'utilisateur', 'fournisseur', 'famille')
 
 class FamilleMaterialAdmin(admin.ModelAdmin):
     list_display = ('nom', 'type')
+
 class FournisseurAdmin(admin.ModelAdmin):
     list_display = ('nom', 'adresse', 'telephone', 'email', 'site_web')
     
