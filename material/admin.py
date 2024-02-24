7ded364b169dc3a9a5f78824724ef41288b96871
houdaaaaaaaaaa <nourgheffari@gmail.com>
2024-02-24 18:00:18 +0100

    1

--- material/admin.py
+++ material/admin.py
@@ -1,34 +1,22 @@
-from django import forms
 from django.contrib import admin
-from django.contrib.auth.models import User
-from users.models import Localisation
-from .models import Material, Famille_Material, Fournisseur
+from .models import*
 
 class MaterialAdmin(admin.ModelAdmin):
-    list_display = ('nom', 'numero_serie', 'marque', 'duree_garantie', 'duree_vie_debut', 'duree_vie_fin', 'date_affectation', 'statut', 'get_user_username', 'fournisseur', 'famille')
-    
-    def get_user_username(self, obj):
-        return obj.utilisateur.username if obj.utilisateur else ""
-    get_user_username.short_description = 'User'
-
-    def save_model(self, request, obj, form, change):
-        if not obj.utilisateur and obj.localisation:
-            obj.utilisateur = User.objects.filter(localisation=obj.localisation).first()
-        super().save_model(request, obj, form, change)
-
     def get_form(self, request, obj=None, **kwargs):
         form = super().get_form(request, obj, **kwargs)
-        if not obj:
-            # If creating a new material, hide the utilisateur field
-            form.base_fields['utilisateur'].widget = forms.HiddenInput()
+        if 'material_type' in form.base_fields:
+            form.base_fields['material_type'].widget.attrs['onchange'] = 'selectMaterialType(this);'
         return form
 
-class FamilleMaterialAdmin(admin.ModelAdmin):
-    list_display = ('nom', 'type')
+    class Media:
+        js = (
+            'material/admin.js',
+        )
 
-class FournisseurAdmin(admin.ModelAdmin):
-    list_display = ('nom', 'adresse', 'telephone', 'email', 'site_web')
-    
-admin.site.register(Material, MaterialAdmin)
-admin.site.register(Famille_Material, FamilleMaterialAdmin)
-admin.site.register(Fournisseur, FournisseurAdmin)
+@admin.register(Hardware)
+class HardwareAdmin(MaterialAdmin):
+    pass
+
+@admin.register(Software)
+class SoftwareAdmin(MaterialAdmin):
+    pass
--- material/models.py
+++ material/models.py
@@ -1,49 +1,51 @@
 from django.db import models
-from django.contrib.auth.models import User
-from users.models import Localisation
 
-class Famille_Material(models.Model): 
-    nom=models.CharField(max_length=300)
-    TYPES_CHOICES = [
-        ('hard', 'Matériel informatique'),
-        ('soft', 'Logiciel'),
-    ]
-    type = models.CharField(max_length=300, choices=TYPES_CHOICES)
-    def __str__(self):
-        return self.nom
+class MaterialType(models.TextChoices):
+    HARDWARE = 'hardware', 'Hardware'
+    SOFTWARE = 'software', 'Software'
 
-class Fournisseur(models.Model):
-    nom = models.CharField(max_length=100)
-    adresse = models.CharField(max_length=200)
-    telephone = models.CharField(max_length=20)
-    email = models.EmailField()
-    site_web = models.URLField(blank=True)
-    def __str__(self):
-        return self.nom
-    
 class Material(models.Model):
-    nom = models.CharField(max_length=100)
-    numero_serie = models.CharField(max_length=100)
-    marque = models.CharField(max_length=100)
-    duree_garantie = models.IntegerField()  # En mois par exemple
-    duree_vie_debut = models.DateField()
-    duree_vie_fin = models.DateField()
-    date_affectation = models.DateField(null=True, blank=True)
-    STATUT_CHOICES = [
-        ('actif', 'Actif'),
-        ('en_panne', 'En panne'),
-        ('en_reparation', 'En réparation'),
-    ]
-    statut = models.CharField(max_length=100, choices=STATUT_CHOICES)
-    utilisateur = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
-    fournisseur = models.ForeignKey(Fournisseur, on_delete=models.CASCADE)
-    famille = models.ForeignKey(Famille_Material, on_delete=models.CASCADE)
-    localisation = models.ForeignKey(Localisation, on_delete=models.CASCADE, null=True, blank=True)
+    material_type = models.CharField(
+        max_length=10,
+        choices=MaterialType.choices,
+        default=MaterialType.HARDWARE,
+    )
 
-    def save(self, *args, **kwargs):
-        if self.localisation and not self.utilisateur:
-            self.utilisateur = self.localisation.user
-        super().save(*args, **kwargs)
+    class Meta:
+        abstract = True
+
+class Hardware(Material):
+    nom = models.CharField(max_length=100)
+    numero_serie = models.CharField(max_length=100, unique=True)
+    fabricant = models.CharField(max_length=100)
+    fournisseur = models.CharField(max_length=100)
+    date_achat = models.DateField()
+    date_garantie_expiration = models.DateField()
+    date_maintenance = models.DateField(blank=True, null=True)
+    etat_choices = [
+        ('actif', 'Actif'),
+        ('en_panne', 'En Panne'),
+        ('en_reparation', 'En Réparation'),
+        ('en_maintenance', 'En Maintenance'),
+        ('obsolete', 'Obsolète'),
+    ]
+    etat = models.CharField(max_length=20, choices=etat_choices)
+    utilisateur_actuel = models.CharField(max_length=100, blank=True, null=True)
+    emplacement = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.nom
+
+class Software(Material):
+    nom = models.CharField(max_length=100)
+    version = models.CharField(max_length=50)
+    licence = models.CharField(max_length=100)
+    date_achat = models.DateField()
+    date_fin_support = models.DateField()
+    equipement_utilise = models.ForeignKey(Hardware, on_delete=models.SET_NULL, blank=True, null=True)
+    utilisateur_actuel = models.CharField(max_length=100, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.nom
