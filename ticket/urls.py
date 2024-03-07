69ac456fa597c25a68a4c29d0a7399867d6cac76
houdaaaaaaaaaa <nourgheffari@gmail.com>
2024-03-07 11:21:24 +0100

    1

--- ticket/urls.py
+++ ticket/urls.py
@@ -6,4 +6,15 @@ urlpatterns = [
 
 path('tech/',views.tech,name="tech"),
 path('admtech/',views.adm_tech,name="adm_tech"),
- ]
+path('list/',views.listtech,name='listtech'),
+path('resod/',views.resoudre,name='resoudre'),
+path('resl/',views.resl,name='resl'),
+path('reseaux/',views.reseaux,name='reseaux'),
+path('logiciel/',views.logiciel,name='logiciel'),
+path('web/',views.web,name='web'),
+path('tecket/',views.tecketresul,name='ticket'),
+path('rehard/',views.rehard,name='rehard'),
+path('logisoft/',views.logisoft,name='logisoft'),
+path('matehard/',views.matehard,name='mtehard'),
+
+]
--- ticket/views.py
+++ ticket/views.py
@@ -6,3 +6,23 @@ def tech(request):
 
 def adm_tech(request):
     return render(request,'Ticket/index-adm.html')
+def rehard(request):
+    return render(request,'Ticket/rehard.html')
+def logisoft(request):
+    return render(request,'Ticket/logisoft.html')
+def matehard(request):
+    return render(request,'Ticket/matehard.html')
+def tecketresul(request):
+    return render(request,'Ticket/tecketresul.html')
+def listtech(request):
+    return render(request,'Ticket/listtech.html')
+def resoudre(request):
+    return render(request,'Ticket/resoudre.html')
+def resl(request):
+    return render(request,'Ticket/resolu.html')
+def reseaux(request):
+    return render(request,'Ticket/reseaux.html')
+def logiciel(request):
+    return render(request,'Ticket/logiciel.html')
+def web (request):
+    return render(request,'Ticket/web.html')
